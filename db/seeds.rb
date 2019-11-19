58b3baf6b96f3f47c958ec804eadf9d776d5db1d
nikolaivi <vnikolaivi@gmail.com>
2019-11-19 21:05:22 +0200

    database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#users_list = [
+#["Petro", "Pukas"],
+#["Vasil", "Dydnik"],
+#["Anastasia", "Kyltchitskaya"],
+#["Tatyna", "Tsarenko"],
+#["Oleg", "Bogachenko"],
+#["Oleksandr", "Taran"],
+#["Nikolai", "Zayats"],
+#["Leonid", "Hlan"],
+#["Natalia", "Borisova"],
+#["Nikolai", "Vidiborenko"]
+#]
+
+#users_list.each do |first_name, last_name|
+#  User.create( first_name: first_name, last_name: last_name )
+#end
+
+User.create( first_name: 'Nikolai', last_name: 'Vidiborenko' )
